4b3e6bd05c9c7aa85a13d8d5ecc4b6f345e1260d
maurighar <maurighar@gmail.com>
2023-06-13 12:51:43 -0300

    Primera aproximación cifado cesar

--- 2023/24_cifrado_cesar.py
+++ 2023/24_cifrado_cesar.py
@@ -7,5 +7,32 @@
 alfabeto_normal = 'abcdefghijklmnopqrstuvwxyz 1234567890'
 alfabeto_cesar  = 'ghijklmnopqrstuvwxyz 1234567890abcdef'
 
-print(alfabeto_cesar[alfabeto_normal.find('g')])
-print(alfabeto_normal[alfabeto_cesar.find('m')])
+print(alfabeto_normal.find('t'))
+# print(alfabeto_normal[alfabeto_cesar.find('m')])
+
+def encode_cesar(message:str) -> str:
+    message_cifer = ''
+    for string in message:
+        index = alfabeto_cesar.find(string)
+        if index >= 0:
+            message_cifer += alfabeto_normal[index]
+        else:
+            message_cifer += '#'
+
+    return message_cifer
+
+def decode_cesar(message:str) -> str:
+    message_clear = ''
+    for string in message:
+        index = alfabeto_normal.find(string)
+        if index >= 0:
+            message_clear += alfabeto_cesar[index]
+        else:
+            message_clear += '#'
+
+    return message_clear
+
+if __name__ == "__main__":
+    print(encode_cesar('te recomiendo que busques informacion para conocer en profundidad'))
+
+    print(decode_cesar('n9ul97igc9h8iuko9u6omko9much0ilg57cihuj5l5u7ihi79lu9hujli0oh8c858'))
