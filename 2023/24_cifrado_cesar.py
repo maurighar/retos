13eb85e8ccc9abf5cb92dd836f16afa6f295ce55
maurighar <maurighar@gmail.com>
2023-06-16 09:12:15 -0300

    Cifrado cesar, agregue una funcion generada por chatGPT

--- 2023/24_cifrado_cesar.py
+++ 2023/24_cifrado_cesar.py
@@ -34,11 +34,36 @@ def decode_cesar(message:str, shift:int) -> str:
 
     return message_clear
 
+def cifrado_cesar(texto, desplazamiento):
+    # Generado chatGPT
+    resultado = ""
+    
+    for caracter in texto:
+        if caracter.isalpha():
+            # Obtener el código ASCII del carácter y ajustar el desplazamiento
+            codigo = ord(caracter) + desplazamiento
+            
+            # Si el código es mayor que el código ASCII de 'Z' o 'z',
+            # volver al inicio del rango al restar 26 (número de letras en el alfabeto)
+            if caracter.isupper() and codigo > ord('Z'):
+                codigo -= 26
+            elif caracter.islower() and codigo > ord('z'):
+                codigo -= 26
+            
+            # Convertir el código ASCII de vuelta a carácter
+            caracter_cifrado = chr(codigo)
+            resultado += caracter_cifrado
+        else:
+            resultado += caracter
+    
+    return resultado
+
 if __name__ == "__main__":
     mensaje = 'te recomiendo que busques informacion para conocer en profundidad'
     mensaje = 'prueba 234, si (?/7Ú\),\n MAYUSCULAS'
     print(encode_cesar(mensaje, 3))
 
     mensaje = 'wh3uhfrplhqgr3txh3exvtxhv3lqirupdflrq3sdud3frqrfhu3hq3surixqglgdg'
-    mensaje = 'suxhed3567#3vl3###0####3pd1xvfxodv'
+    # mensaje = 'suxhed3567#3vl3###0####3pd1xvfxodv'
     print(decode_cesar(mensaje, 3))
+    print(cifrado_cesar(mensaje, -3))
